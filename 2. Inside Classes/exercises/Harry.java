9f05571a3c95e20cc861605a224c17ee8cfaa367
Iamniss1234 <thilakshananiss1234@gmail.com>
2023-07-27 14:07:30 +0530

    commented all

--- 2. Inside Classes/exercises/Harry.java
+++ 2. Inside Classes/exercises/Harry.java
@@ -1,36 +1,36 @@
-package exercises;
+// package exercises;
 
-public class Harry {
+// public class Harry {
 
-	private boolean cloakOn;
+// 	private boolean cloakOn;
 
-	Harry() {
-		System.out.println("making Harry Potter...");
-	}
+// 	Harry() {
+// 		System.out.println("making Harry Potter...");
+// 	}
 
-	void castSpell(String spell) {
-		System.out.println("casting spell: " + spell);
-	}
+// 	void castSpell(String spell) {
+// 		System.out.println("casting spell: " + spell);
+// 	}
 
-	void makeInvisible(boolean invisible) {
-		this.cloakOn = invisible;
+// 	void makeInvisible(boolean invisible) {
+// 		this.cloakOn = invisible;
 
-		if (cloakOn)
-			System.out.println("Harry is invisible");
-		else
-			System.out.println("Harry is visible");
-	}
+// 		if (cloakOn)
+// 			System.out.println("Harry is invisible");
+// 		else
+// 			System.out.println("Harry is visible");
+// 	}
 
-	void spyOnSnape() {
-		System.out.println("Harry sees Professor Snape doing nefarious things.");
-	}
+// 	void spyOnSnape() {
+// 		System.out.println("Harry sees Professor Snape doing nefarious things.");
+// 	}
 
-	public static void main(String[] args) {
-		// 1. make harry potter
-		// 2. become invisible
-		// 3. spy on professor snape
-		// 4. become visible again
-		// 5. cast a “stupefy” spell
-	}
+// 	public static void main(String[] args) {
+// 		// 1. make harry potter
+// 		// 2. become invisible
+// 		// 3. spy on professor snape
+// 		// 4. become visible again
+// 		// 5. cast a “stupefy” spell
+// 	}
 
-}
+// }
